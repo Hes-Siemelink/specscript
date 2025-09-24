8377f38a4cc073c8ecff81b1c26323b6a54814b4
Hes Siemelink <hsiemelink@digital.ai>
2025-09-24 21:22:43 +0200

    💫  'Connect to' only runs connection scripts once

--- src/main/kotlin/specscript/commands/connections/ConnectTo.kt
+++ src/main/kotlin/specscript/commands/connections/ConnectTo.kt
@@ -9,25 +9,46 @@ import kotlin.io.path.name
 
 object ConnectTo : CommandHandler("Connect to", "core/connections"), ValueHandler {
 
+    val alreadyConnected: MutableMap<String, JsonNode?> = mutableMapOf()
+
     override fun execute(data: ValueNode, context: ScriptContext): JsonNode? {
 
         if (context !is FileContext) {
             error("'Connect to' is only supported when running files.")
         }
 
-        val targetName = data.textValue()
-        val connectScript = context.info.connections[targetName]
-            ?: throw ScriptingException("No connection script configured for $targetName in ${context.scriptFile.parent.toRealPath().name}")
+        // Already connected, return previous result from connection script
+        val target = data.textValue()
+        if (alreadyConnected.contains(target)) {
+            return alreadyConnected[target]
+        }
 
-        when (connectScript) {
-            is ValueNode -> {
-                val cliFile = context.scriptDir.resolve(connectScript.textValue())
-                return SpecScriptFile(cliFile).run(FileContext(cliFile, context))
-            }
+        // Find script
+        val connectScript = context.info.connections[target]
+            ?: throw ScriptingException("No connection script configured for $target in ${context.scriptFile.parent.toRealPath().name}")
 
-            else -> {
-                return connectScript.run(context)
-            }
+        // Execute script
+        val result = connect(connectScript, context)
+
+        alreadyConnected[target] = result
+
+        return result
+    }
+}
+
+private fun connect(
+    connectScript: JsonNode,
+    context: FileContext
+): JsonNode? {
+
+    when (connectScript) {
+        is ValueNode -> {
+            val cliFile = context.scriptDir.resolve(connectScript.textValue())
+            return SpecScriptFile(cliFile).run(FileContext(cliFile, context))
+        }
+
+        else -> {
+            return connectScript.run(context)
         }
     }
 }
